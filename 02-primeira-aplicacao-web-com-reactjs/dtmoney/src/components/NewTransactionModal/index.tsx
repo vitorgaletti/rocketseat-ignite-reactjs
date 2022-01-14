89ae90bb51bc777cceee2157332600cabac1690e
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-01-14 17:13:40 -0300

    :lipstick: Button colors in the component NewTransactionModal

--- 02-primeira-aplicacao-web-com-reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
+++ 02-primeira-aplicacao-web-com-reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -46,6 +46,7 @@ export function NewTransactionModal({
               setType('deposit');
             }}
             isActive={type === 'deposit'}
+            activeColor="green"
           >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
@@ -57,6 +58,7 @@ export function NewTransactionModal({
               setType('withdraw');
             }}
             isActive={type === 'withdraw'}
+            activeColor="red"
           >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
