3d006ca4bf821ff71a00445aa2cc32ad12a65f05
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-01-14 17:53:00 -0300

    :sparkles: Insert data of component NewTransactionModal in Api

--- 02-primeira-aplicacao-web-com-reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
+++ 02-primeira-aplicacao-web-com-reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 
+import { api } from '../../services/api';
+
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
@@ -25,7 +27,9 @@ export function NewTransactionModal({
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({ title, category, type, value });
+    const data = { title, value, category, type };
+
+    api.post('/transactions', data);
   }
 
   return (
