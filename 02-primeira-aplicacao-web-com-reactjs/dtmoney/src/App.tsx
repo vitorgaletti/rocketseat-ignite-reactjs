bd1ae94890f35d09ede5412279dc2482c73cd0d5
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-01-15 12:02:27 -0300

    :construction: Context Api in React

--- 02-primeira-aplicacao-web-com-reactjs/dtmoney/src/App.tsx
+++ 02-primeira-aplicacao-web-com-reactjs/dtmoney/src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
+
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './TransactionsContext';
+
 import { GlobalStyle } from './styles/global';
 
 Modal.setAppElement('#root');
@@ -20,7 +23,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -31,6 +34,6 @@ export function App() {
       />
 
       <GlobalStyle></GlobalStyle>
-    </>
+    </TransactionsProvider>
   );
 }
