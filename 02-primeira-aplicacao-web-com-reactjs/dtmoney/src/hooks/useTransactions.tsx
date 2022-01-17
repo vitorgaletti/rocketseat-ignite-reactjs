5fe75a710ae76b0ca2dbeffffc7a351d723ffc04
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-01-17 15:08:36 -0300

    :art: Created useTransactions hooks

--- 02-primeira-aplicacao-web-com-reactjs/dtmoney/src/hooks/useTransactions.tsx
+++ 02-primeira-aplicacao-web-com-reactjs/dtmoney/src/hooks/useTransactions.tsx
@@ -1,5 +1,11 @@
-import { createContext, useEffect, useState, ReactNode } from 'react';
-import { api } from './services/api';
+import {
+  createContext,
+  useEffect,
+  useState,
+  ReactNode,
+  useContext
+} from 'react';
+import { api } from '../services/api';
 
 interface Transaction {
   id: number;
@@ -49,4 +55,10 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       {children}
     </TransactionsContext.Provider>
   );
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+
+  return context;
 }
