d0b84a7512ffffd9feee1fa853c41fedea9bab5f
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-01-20 17:47:19 -0300

    :lipstick: Add global.scss

--- 03-fundamentos-do-nextjs/ignews/src/pages/index.tsx
+++ 03-fundamentos-do-nextjs/ignews/src/pages/index.tsx
@@ -1,9 +1,14 @@
-import styles from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello <span>World</span>
-    </h1>
+    <>
+      <Head>
+        <title>Ínicio | ig.news</title>
+      </Head>
+      <h1>
+        Hello <span>World</span>
+      </h1>
+    </>
   );
 }
