d973bef1d3bfa2c47e5f04af14564bacbd87dcb6
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-02-01 15:45:23 -0300

    :construction: Generating checkout session in Stripe

--- 03-fundamentos-do-nextjs/ignews/src/components/SubscribeButton/index.tsx
+++ 03-fundamentos-do-nextjs/ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { useSession, signIn } from 'next-auth/react';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,8 +6,20 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+  }
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
