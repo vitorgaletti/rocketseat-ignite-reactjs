e763744ad338293f020a1c404c639014786536dd
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-02-01 00:44:11 -0300

    :sparkles: Saving user data on FaunaDB

--- 03-fundamentos-do-nextjs/ignews/src/pages/api/auth/[...nextauth].ts
+++ 03-fundamentos-do-nextjs/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -8,5 +12,38 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: { params: { scope: 'read:user' } }
     })
-  ]
+  ],
+
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('user_by_email'),
+                  query.Casefold(user.email)
+                )
+              )
+            ),
+            query.Create(query.Collection('users'), { data: { email } }),
+            query.Get(
+              query.Match(
+                query.Index('user_by_email'),
+                query.Casefold(user.email)
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    }
+  }
 });
