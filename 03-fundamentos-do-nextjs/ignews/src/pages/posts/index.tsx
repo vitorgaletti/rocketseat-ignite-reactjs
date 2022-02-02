5013eb9d2f4c76ebf2941321e9187a595d192ede
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-02-02 18:22:00 -0300

    :construction: Consuming Prismic API

--- 03-fundamentos-do-nextjs/ignews/src/pages/posts/index.tsx
+++ 03-fundamentos-do-nextjs/ignews/src/pages/posts/index.tsx
@@ -1,5 +1,10 @@
 import Head from 'next/head';
+
+import Prismic from '@prismicio/client';
+import { getPrismicClient } from '../../services/prismic';
+
 import styles from './styles.module.scss';
+import { GetStaticProps } from 'next';
 
 export default function Posts() {
   return (
@@ -41,3 +46,21 @@ export default function Posts() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'publication')],
+    {
+      fetch: ['publication.title', 'publication.content'],
+      pageSize: 100
+    }
+  );
+
+  console.log(JSON.stringify(response, null, 2));
+
+  return {
+    props: {}
+  };
+};
---
+++ 03-fundamentos-do-nextjs/ignews/src/services/prismic.ts
@@ -0,0 +1,9 @@
+import Prismic from '@prismicio/client';
+
+export function getPrismicClient() {
+  const prismic = Prismic.client(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACESS_TOKEN
+  });
+
+  return prismic;
+}
