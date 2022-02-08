e20153f93a05b767a6d9d714169ea71f4e656958
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-02-08 01:51:11 -0300

    :construction: Add Page post Preview

--- 03-fundamentos-do-nextjs/ignews/src/pages/posts/[slug].tsx
+++ 03-fundamentos-do-nextjs/ignews/src/pages/posts/[slug].tsx
@@ -43,7 +43,7 @@ export const getServerSideProps: GetServerSideProps = async ({
   const session = await getSession({ req });
   const { slug } = params;
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
