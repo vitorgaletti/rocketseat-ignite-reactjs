d6f3678d1b9c4e2568535488a8a71dbc1945e317
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-02-18 17:43:40 -0300

    :sparkles: Finishing Header Component

--- 04-construindo-interfaces-do-futuro/dashgo/src/components/Header.tsx
+++ 04-construindo-interfaces-do-futuro/dashgo/src/components/Header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine
+} from 'react-icons/ri';
 
 export function Header() {
   return (
@@ -41,6 +45,36 @@ export function Header() {
           _placeholder={{ color: 'gray.400' }}
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20"></Icon>
+          <Icon as={RiUserAddLine} fontSize="20"></Icon>
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Vitor Galeti</Text>
+            <Text color="gray.300" fontSize="small">
+              vitor.fgaletti@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Vitor Galeti"
+            src="https://github.com/vitorgaletti.png"
+          ></Avatar>
+        </Flex>
       </Flex>
     </Flex>
   );
