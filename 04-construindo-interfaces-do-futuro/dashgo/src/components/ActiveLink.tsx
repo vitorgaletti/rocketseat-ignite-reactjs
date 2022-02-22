8df74f5f86cf9289f09c2d6bead41d78cbc25cad
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-02-22 15:09:19 -0300

    :sparkles: Create ActiveLink

---
+++ 04-construindo-interfaces-do-futuro/dashgo/src/components/ActiveLink.tsx
@@ -0,0 +1,36 @@
+import Link, { LinkProps } from 'next/link';
+import { useRouter } from 'next/router';
+import { cloneElement, ReactElement } from 'react';
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement;
+  shouldMathExactHref?: boolean;
+}
+
+export function ActiveLink({
+  children,
+  shouldMathExactHref,
+  ...rest
+}: ActiveLinkProps) {
+  const { asPath } = useRouter();
+  let isActive = false;
+
+  if (shouldMathExactHref && (asPath === rest.href || asPath === rest.as)) {
+    isActive = true;
+  }
+
+  if (
+    (!shouldMathExactHref && asPath.startsWith(String(rest.href))) ||
+    asPath.startsWith(String(rest.as))
+  ) {
+    isActive = true;
+  }
+
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        color: isActive ? 'pink.400' : 'gray.50'
+      })}
+    </Link>
+  );
+}
--- 04-construindo-interfaces-do-futuro/dashgo/src/components/Sidebar/NavLink.tsx
+++ 04-construindo-interfaces-do-futuro/dashgo/src/components/Sidebar/NavLink.tsx
@@ -4,8 +4,9 @@ import {
   Text,
   LinkProps as ChakraLinkProps
 } from '@chakra-ui/react';
-import Link from 'next/link';
+
 import { ElementType } from 'react';
+import { ActiveLink } from '../ActiveLink';
 
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType;
@@ -15,13 +16,13 @@ interface NavLinkProps extends ChakraLinkProps {
 
 export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" alignItems="flex-start" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">
           {children}
         </Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   );
 }
