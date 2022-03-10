c1c4d07c7480f56cbc66fd78c6d5f03b93a33430
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-03-10 17:56:30 -0300

    :construction: Factories and seeds MirageJS

--- 04-construindo-interfaces-do-futuro/dashgo/src/pages/users/index.tsx
+++ 04-construindo-interfaces-do-futuro/dashgo/src/pages/users/index.tsx
@@ -15,7 +15,8 @@ import {
   useBreakpointValue
 } from '@chakra-ui/react';
 import Link from 'next/link';
-import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import { useEffect } from 'react';
+import { RiAddLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
@@ -25,6 +26,12 @@ export default function UserList() {
     base: false,
     lg: true
   });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, []);
 
   return (
     <Box>
