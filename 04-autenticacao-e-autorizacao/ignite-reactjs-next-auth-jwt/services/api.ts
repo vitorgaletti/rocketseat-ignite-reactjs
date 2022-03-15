8a40370c763d7e9de2e5748bda9f80e1261beb02
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-03-15 16:41:44 -0300

    :construction: Refreshing the token

--- 04-autenticacao-e-autorizacao/ignite-reactjs-next-auth-jwt/services/api.ts
+++ 04-autenticacao-e-autorizacao/ignite-reactjs-next-auth-jwt/services/api.ts
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
-import { parseCookies } from 'nookies';
+import { parseCookies, setCookie } from 'nookies';
 
-const cookies = parseCookies();
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -10,3 +10,44 @@ export const api = axios.create({
     Authorization: `Bearer ${cookies['nextauth.token']}`
   }
 });
+
+api.interceptors.response.use(
+  response => {
+    return response;
+  },
+  (error: AxiosError) => {
+    if (error.response.status === 401) {
+      if (error.response.data?.code === 'token.expired') {
+        cookies = parseCookies();
+
+        const { 'nextauth.refreshToken': refreshToken } = cookies;
+
+        api
+          .post('/refresh', {
+            refreshToken
+          })
+          .then(response => {
+            const { token } = response.data;
+
+            setCookie(undefined, 'nextauth.token', token, {
+              maxAge: 60 * 60 * 24 * 30,
+              path: '/'
+            });
+
+            setCookie(
+              undefined,
+              'nextauth.refreshToken',
+              response.data.refreshToken,
+              {
+                maxAge: 60 * 60 * 24 * 30,
+                path: '/'
+              }
+            );
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`;
+          });
+      } else {
+      }
+    }
+  }
+);
