f23eb43f00c296aa16286a2f0ae70e0ac900bae4
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-03-15 11:50:48 -0300

    :construction: Saving user data

--- 04-autenticacao-e-autorizacao/ignite-reactjs-next-auth-jwt/contexts/AuthContext.tsx
+++ 04-autenticacao-e-autorizacao/ignite-reactjs-next-auth-jwt/contexts/AuthContext.tsx
@@ -1,4 +1,13 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useState } from 'react';
+import Router from 'next/router';
+
+import { api } from '../services/api';
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 
 type SignInCredentials = {
   email: string;
@@ -7,6 +16,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
   isAuthenticated: boolean;
 };
 
@@ -17,14 +27,33 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>();
+
+  const isAuthenticated = !!user;
 
   async function signIn({ email, password }: SignInCredentials) {
-    console.log(email, password);
+    try {
+      const response = await api.post('sessions', {
+        email,
+        password
+      });
+
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setUser({
+        email,
+        permissions,
+        roles
+      });
+
+      Router.push('/dashboard');
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   );
