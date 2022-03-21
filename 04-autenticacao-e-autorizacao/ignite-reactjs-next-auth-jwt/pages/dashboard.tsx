ac615c467db54c3b8d538bb50db91f3d912f260e
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-03-21 15:28:10 -0300

    :construction: Retrieving token on the server-side

--- 04-autenticacao-e-autorizacao/ignite-reactjs-next-auth-jwt/pages/dashboard.tsx
+++ 04-autenticacao-e-autorizacao/ignite-reactjs-next-auth-jwt/pages/dashboard.tsx
@@ -6,7 +6,10 @@ export default function Dashboard() {
   const { user } = useContext(AuthContext);
 
   useEffect(() => {
-    api.get('/me').then(response => console.log(response));
+    api
+      .get('/me')
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
   }, []);
   return <h1>Dashboard: {user?.email}</h1>;
 }
--- 04-autenticacao-e-autorizacao/ignite-reactjs-next-auth-jwt/pages/index.tsx
+++ 04-autenticacao-e-autorizacao/ignite-reactjs-next-auth-jwt/pages/index.tsx
@@ -1,4 +1,6 @@
+import { GetServerSideProps } from 'next';
 import { FormEvent, useContext, useState } from 'react';
+import { parseCookies } from 'nookies';
 import { AuthContext } from '../contexts/AuthContext';
 import styles from '../styles/Home.module.css';
 
@@ -36,3 +38,20 @@ export default function Home() {
     </form>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const cookies = parseCookies(ctx);
+
+  if (cookies['nextauth.token']) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false
+      }
+    };
+  }
+
+  return {
+    props: {}
+  };
+};
