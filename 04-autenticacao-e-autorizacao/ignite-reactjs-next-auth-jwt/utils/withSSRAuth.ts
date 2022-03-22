7954bd79c16909b80d24de53ce91b22c1d2e582b
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-03-22 16:45:12 -0300

    :sparkles: Validating permissions (server)

--- 04-autenticacao-e-autorizacao/ignite-reactjs-next-auth-jwt/utils/withSSRAuth.ts
+++ 04-autenticacao-e-autorizacao/ignite-reactjs-next-auth-jwt/utils/withSSRAuth.ts
@@ -5,20 +5,51 @@ import {
 } from 'next';
 import { destroyCookie, parseCookies } from 'nookies';
 import { AuthTokenError } from '../services/errors/AuthTokenError';
+import decode from 'jwt-decode';
+import { validateUserPermissions } from './validateUserPermissions';
 
-export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
+type withSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+};
+
+export function withSSRAuth<P>(
+  fn: GetServerSideProps<P>,
+  options?: withSSRAuthOptions
+) {
   return async (
     ctx: GetServerSidePropsContext
   ): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(ctx);
+    const token = cookies['nextauth.token'];
 
-    if (!cookies['nextauth.token']) {
+    if (!token) {
       return {
         redirect: {
           destination: '/',
           permanent: false
         }
       };
+    }
+
+    if (options) {
+      const user = decode<{ permissions: string[]; roles: string[] }>(token);
+      const { permissions, roles } = options;
+
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles
+      });
+
+      if (!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: '/dashboard',
+            permanent: false
+          }
+        };
+      }
     }
 
     try {
