0cb4443122e2202d373138c82552da81afbcafdb
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-03-24 16:18:26 -0300

    :construction: memo

--- 05-perfomando-apps-com-reactjs/perfreact/src/components/SearchResults.tsx
+++ 05-perfomando-apps-com-reactjs/perfreact/src/components/SearchResults.tsx
@@ -17,3 +17,17 @@ export function SearchResults({ results }: SearchResultsProps) {
     </div>
   );
 }
+
+/**
+ * 1. Criar uma nova versão do componente
+ * 2. Comparar com a versão anterior
+ * 3. Se houverem alterações, vai atualizar o que alterou
+ */
+
+// Uso do memo
+/**
+ * 1. Pure functional Components
+ * 2. Reenders too often
+ * 3. Re-renders with same props
+ * 4. Medium to big size
+ */
