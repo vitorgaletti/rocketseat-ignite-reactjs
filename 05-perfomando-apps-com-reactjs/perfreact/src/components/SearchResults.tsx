7199e80830b1a1a407e87bdf43d7c717a6c85786
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-03-24 16:32:14 -0300

    :construction: useMemo

--- 05-perfomando-apps-com-reactjs/perfreact/src/components/SearchResults.tsx
+++ 05-perfomando-apps-com-reactjs/perfreact/src/components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { ProductItem } from './ProductItem';
 
 interface SearchResultsProps {
@@ -9,8 +10,15 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }, [results]);
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
       {results.map(product => {
         return <ProductItem product={product} key={product.id} />;
       })}
@@ -30,4 +38,10 @@ export function SearchResults({ results }: SearchResultsProps) {
  * 2. Reenders too often
  * 3. Re-renders with same props
  * 4. Medium to big size
+ */
+
+// useMemo
+/**
+ * 1. Cálculos pesados
+ * 2. Igualdade referencial (quando a gente repassa aquela informação a um componente filho)
  */
