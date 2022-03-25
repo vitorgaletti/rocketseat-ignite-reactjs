2587e651bfba94f193c0ca1b9af8835792c069bc
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-03-25 11:35:21 -0300

    :construction: Virtualized

--- 05-perfomando-apps-com-reactjs/perfreact/src/components/SearchResults.tsx
+++ 05-perfomando-apps-com-reactjs/perfreact/src/components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { List, ListRowRenderer } from 'react-virtualized';
 import { ProductItem } from './ProductItem';
 
 interface SearchResultsProps {
@@ -17,10 +17,31 @@ export function SearchResults({
   results,
   onAddToWishList
 }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map(product => {
+
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+
+      {/* {results.map(product => {
         return (
           <ProductItem
             key={product.id}
@@ -28,7 +49,7 @@ export function SearchResults({
             onAddToWishList={onAddToWishList}
           />
         );
-      })}
+      })} */}
     </div>
   );
 }
