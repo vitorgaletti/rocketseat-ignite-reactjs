ed1874b2f26a9c4f849d486df78f12ea32ee9b0f
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-03-28 16:58:48 -0300

    :test_tube: Testing header

--- 05-testes-unitarios-no-react/ignews/jest.config.js
+++ 05-testes-unitarios-no-react/ignews/jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
   },
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  moduleNameMapper: { '\\.(scss|css|sass)': 'identity-obj-proxy' }
 };
