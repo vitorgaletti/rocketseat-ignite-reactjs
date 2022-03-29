ef1c8123a6b78d4f879eb35c586a9e3ff8021c86
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-03-29 16:06:40 -0300

    :white_check_mark: Testing home page

--- 05-testes-unitarios-no-react/ignews/src/pages/index.tsx
+++ 05-testes-unitarios-no-react/ignews/src/pages/index.tsx
@@ -14,7 +14,7 @@ import styles from './home.module.scss';
 interface HomeProps {
   product: {
     priceId: string;
-    amount: number;
+    amount: string;
   };
 }
 
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps) {
             Get acess to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
